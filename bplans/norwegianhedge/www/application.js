ebed360f0969f5d203e2c16cc24323d782fa49f7
anon987654321 <oowae5a@gmail.com>
2025-07-18 10:32:35 +0200

    Initial commit

---
+++ bplans/norwegianhedge/www/application.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+
+  // Kjør kode kun på forsiden
+
+  if($("#btc").length) {
+    const fiatCurrency = "NOK";
+    let conversionObj;
+    let rate;
+  
+    function getConversionRate() {
+      return $.ajax({
+        dataType: "json",
+      
+        // Use reverse proxy due to lack of CORS (http://enable-cors.org/)
+      
+        // https://github.com/Rob--W/cors-anywhere
+      
+        // url: "https://cors.riksmynt.no/localbitcoins.com/bitcoinaverage/ticker-all-currencies/",
+        url: "https://cors-anywhere.herokuapp.com/localbitcoins.com/bitcoinaverage/ticker-all-currencies/",
+        success: function(data) {
+     
+          // Parse JSON for rates
+          
+          rate = data[fiatCurrency].rates.last;
+        }
+      });
+    }
+  
+    // Initialize rate, then call conversion for initial display
+  
+    getConversionRate().then(bitcoinToFiat);
+  
+    function fiatToBitcoin() {
+      const number = Number($("#fiat").val());
+      $("#btc").val((number / rate).toFixed(4));
+    }
+  
+    function bitcoinToFiat() {
+      const number = Number($("#btc").val());
+      $("#fiat").val((number * rate).toFixed(2));
+    }
+  
+    $("input").keyup(function() {
+      if ($(this).is("input#btc")) {
+        bitcoinToFiat();
+      } else if ($(this).is("input#fiat")) {
+        fiatToBitcoin();
+      }
+    });
+  
+    $("input").click(function() {
+      $(this).select();
+    });
+  
+    // Copy fiat to HTML
+  
+    $('label[for="fiat"]').html(fiatCurrency);
+  }
+});
